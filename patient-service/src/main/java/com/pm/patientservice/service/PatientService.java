c675b88acd94509c1d159c523c7e9407e0ede08e
Bhimesh Ganji <bhimeshganji5@gmail.com>
2026-05-25 13:02:52 +0530

    third comit

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -44,7 +44,7 @@ public class PatientService {
                         new PatientNotFoundException(
                                 "Patient not found with id " + id));
 
-        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+        if (patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(),id)) {
             throw new EmailAlreadyExistsException("A patient with this email " + "already exists in the system " + patientRequestDTO.getEmail());
         }
         patient.setName(patientRequestDTO.getName());
